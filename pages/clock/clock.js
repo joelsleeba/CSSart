fdf4fd2bb2edfc765acdaee0cf78e7a81c4d3b48
joelsleeba <joelsleeba1@gmail.com>
2022-06-11 18:17:49 +0530

    night added, clock has dynamic canvas

--- pages/clock/clock.js
+++ pages/clock/clock.js
@@ -1,8 +1,8 @@
-function setup() {
-    createCanvas(windowWidth, windowHeight);
+function setup() { 
   }
   
   function draw() {
+    createCanvas(windowWidth, windowHeight);
     background(0);
     noFill();
     
